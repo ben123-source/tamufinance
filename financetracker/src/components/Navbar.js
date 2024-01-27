43d7cff5bd97679bd041613c4c091c1c86bff9ed
Ben Tuason <benjosedaa@gmail.com>
2024-01-27 17:40:47 -0600

    setup buttons

---
+++ financetracker/src/components/Navbar.js
@@ -0,0 +1,31 @@
+import { useNavigate } from "react-router-dom"
+
+
+const Navbar = () => {
+    const navigate = useNavigate();
+
+    return(
+        <div>
+            <div className='bg-[#B2A59B] w-screen h-20  flex justify-between items-center'>
+                <div className='flex items-center justify-center gap-4 ml-6'>
+                    
+                </div>
+                <div className='flex items-center justify-center gap-4 mr-6'>
+                <ul className='flex gap-6 mr-4'>
+                    <button onClick={() => navigate("/")} className='hover:underline text-black hover:cursor=pointer'>Home</button>
+                    <button onClick={() => navigate("/")} className='hover:underline text-black hover:cursor=pointer'>Home</button>
+                    <button onClick={() => navigate("/")} className='hover:underline text-black hover:cursor=pointer'>Home</button>
+                    <button onClick={() => navigate("/")} className='hover:underline text-black hover:cursor=pointer'>Home</button>
+                    <button onClick={() => navigate("/")} className='hover:underline text-black hover:cursor=pointer'>Home</button>
+                    <button onClick={() => navigate("/")} className='hover:underline text-black hover:cursor=pointer'>Home</button>
+                    <button onClick={() => navigate("/")} className='hover:underline text-black hover:cursor=pointer'>Home</button>
+
+                </ul>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
