688c28f1b55ef0b16e417567efab9ddaf186cc28
Michael Banes <mbanes05@icloud.com>
2024-01-27 20:55:00 -0600

    progress on sign in page

--- financetracker/src/pages/Home.js
+++ financetracker/src/pages/Home.js
@@ -5,27 +5,25 @@ const Home = () => {
     return (
         <div>
             <Navbar />
-            <form>
-                <h1 class="text-3xl font-bold underline mb-4">Sign Up</h1>
-                <p>Please fill in this form to create an account.</p>
-                <hr></hr>
+            <div class="rounded-lg p-10 ">
+                <form class ="grid grid-cols-2 gap-0">
+                    <div class="bg-lime-100 rounded-lg">
+                        <h1 class="text-3xl font-bold underline mb-4 flex justify-">Sign Up</h1>
 
-                <label for="email"><b>Email</b></label>
-                <input type="text" placeholder="Enter Email" name="email" required></input>
+                        <label for="email "><b>Email</b></label>
+                        <input type="text" placeholder="Enter Email" name="email" required class="rounded-lg"></input>
 
-                <label for="psw"><b>Password</b></label>
-                <input type="password" placeholder="Enter Password" name="psw" required></input>
+                        <p></p>
 
-                <label for="psw-repeat"><b>Repeat Password</b></label>
-                <input type="password" placeholder="Repeat Password" name="psw" required></input>
+                        <label for="psw"><b>Password</b></label>
+                        <input type="password" placeholder="Enter Password" name="psw" required class="rounded-lg"></input>
 
-                <label for="checkbox" checked="checked" name="remember"></label>
-
-                <div class="clearfix">
-                    <button type="button" class="cancelbtn">Cancel</button>
-                    <button type="submit" class="signupbtn">Sign Up</button>
-                </div>
-            </form>
+                        <div class="clearfix">
+                            <button type="submit" class="signupbtn">Sign Up</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
