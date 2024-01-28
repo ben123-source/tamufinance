3cab24ca67c190716b762bf7647987d7fbd45792
Ben Tuason <benjosedaa@gmail.com>
2024-01-27 18:38:45 -0600

    setup

--- financetracker/src/pages/about.js
+++ financetracker/src/pages/about.js
@@ -5,7 +5,29 @@ const About = () => {
     return (
         <div>
             <Navbar />
-            <h1>About</h1>
+            <h1 className="text-3xl font-bold underline mb-4">My Finances</h1>
+            <div className="flex flex-wrap -m-2">
+                <div className="p-2 w-1/2">
+                    <div className="bg-blue-100 p-6 rounded-lg shadow-md">
+                        Cards
+                    </div>
+                </div>
+                <div className="p-2 w-1/2">
+                    <div className="bg-blue-100 p-6 rounded-lg shadow-md">
+                        Transaction History
+                    </div>
+                </div>
+                <div className="p-2 w-1/2">
+                    <div className="bg-blue-100 p-6 rounded-lg shadow-md">
+                        Income
+                    </div>
+                </div>
+                <div className="p-2 w-1/2">
+                    <div className="bg-blue-100 p-6 rounded-lg shadow-md">
+                        Expenses
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- financetracker/src/pages/calendar.js
+++ financetracker/src/pages/calendar.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const Calendar = () => {
+    const [selectedDate, setSelectedDate] = useState(null);
+
+    const handleDateChange = (date) => {
+        // Here you could also handle the logic of storing the due date for the bill
+        setSelectedDate(date);
+    }
+
     return (
         <div>
             <Navbar />
-            <h1>Calendar</h1>
+            <h1 className="text-2xl font-bold mt-4 mb-4">Calendar</h1>
+            <div className="container mx-auto p-4">
+                <DatePicker
+                    selected={selectedDate}
+                    onChange={handleDateChange}
+                    inline
+                />
+            </div>
         </div>
     );
 }
