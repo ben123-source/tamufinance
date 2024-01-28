925db089449c0db21c6f77c00aa94503de5492f7
Michael Banes <mbanes05@icloud.com>
2024-01-28 00:56:08 -0600

    More changes to Sign Up

--- financetracker/src/pages/Home.js
+++ financetracker/src/pages/Home.js
@@ -5,18 +5,18 @@ const Home = () => {
     return (
         <div>
             <Navbar />
-                    <div class="bg-lime-100 rounded-lg p-10">
+                    <div class="bg-gradient-to-r from-lime-100 to-lime-100 p-10 p-4 border-4">
                         <form>
                             <div class="text-center rounded-lg">
-                                <h1 class="text-3xl font-bold underline mb-4">Sign Up/Login</h1>
+                                <h1 class="text-3xl font-bold underline mb-4">Sign Up</h1>
 
-                                <label for="email "><b>Email</b></label>
+                                <label for="email "><b>Email: </b></label>
                                 <input type="text" placeholder="Enter Email" name="email" required class="bg-gray-100 rounded-lg drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]"></input>
 
                                 <br></br>
                                 <br></br>
 
-                                <label for="psw"><b>Password</b></label>
+                                <label for="psw"><b>Password: </b></label>
                                 <input type="password" placeholder="Enter Password" name="psw" required class="bg-gray-100 rounded-lg drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]"></input>
                             
                                 <br></br>
@@ -24,11 +24,15 @@ const Home = () => {
 
                                 <p class="text-xs hover:underline">Forgot your password?</p>
 
-                                <img src="logo512.png" alt="" class="object-fill h-48 w-96" object-center></img>
+                                <br></br>
+
+                                <div>
+                                <img src="istockphoto-1053287264-612x612.jpg" alt="" class="bg-gray-200 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]md:rounded-none rounded-full mx-auto h-30 w-30" object-center></img>
+                                </div>
 
                                 <br></br>
 
-                                <div class="bg-gray-200 rounded-lg drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] text-center">
+                                <div class="bg-gray-200 rounded-lg drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] md:w-14 md:h-15md:rounded-none rounded-full mx-auto">
                                     <button type="submit" class="signupbtn">Sign Up</button>
                                 </div>
                             </div>
