63b1c2b7e09f01481fc5d3e123ad534e5718508b
Michael Banes <mbanes05@icloud.com>
2024-01-27 22:10:26 -0600

    added more to signup page

--- financetracker/src/pages/Home.js
+++ financetracker/src/pages/Home.js
@@ -5,25 +5,38 @@ const Home = () => {
     return (
         <div>
             <Navbar />
-            <div class="rounded-lg p-10 ">
-                <form class ="grid grid-cols-2 gap-0">
-                    <div class="bg-lime-100 rounded-lg">
-                        <h1 class="text-3xl font-bold underline mb-4 flex justify-">Sign Up</h1>
+                    <div class="bg-lime-100 rounded-lg p-10">
+                        <form>
+                            <div class="text-center rounded-lg">
+                                <h1 class="text-3xl font-bold underline mb-4">Sign Up/Login</h1>
 
-                        <label for="email "><b>Email</b></label>
-                        <input type="text" placeholder="Enter Email" name="email" required class="rounded-lg"></input>
+                                <label for="email "><b>Email</b></label>
+                                <input type="text" placeholder="Enter Email" name="email" required class="bg-gray-100 rounded-lg drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]"></input>
 
-                        <p></p>
+                                <br></br>
+                                <br></br>
 
-                        <label for="psw"><b>Password</b></label>
-                        <input type="password" placeholder="Enter Password" name="psw" required class="rounded-lg"></input>
+                                <label for="psw"><b>Password</b></label>
+                                <input type="password" placeholder="Enter Password" name="psw" required class="bg-gray-100 rounded-lg drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]"></input>
+                            
+                                <br></br>
+                                <br></br>
 
-                        <div class="clearfix">
-                            <button type="submit" class="signupbtn">Sign Up</button>
-                        </div>
+                                <p class="text-xs hover:underline">Forgot your password?</p>
+
+                                <img src="logo512.png" alt="" class="object-fill h-48 w-96" object-center></img>
+
+                                <br></br>
+
+                                <div class="bg-gray-200 rounded-lg drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] text-center">
+                                    <button type="submit" class="signupbtn">Sign Up</button>
+                                </div>
+                            </div>
+
+                            <br></br>
+                            <p class="text-xs hover:underline text-center">Don't have an account? Create</p>
+                        </form>
                     </div>
-                </form>
-            </div>
         </div>
     );
 }
